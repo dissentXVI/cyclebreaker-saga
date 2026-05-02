b3802d8e666063e744371ac4e0b101b21e7515f6
dissentXVI <klimerick1369@gmail.com>
2026-05-02 17:55:19 -0500

    Custom 404

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -1,16 +1,14 @@
-import { i18n } from "../../i18n"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types"
 
 const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
-  // If baseUrl contains a pathname after the domain, use this as the home link
   const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
   const baseDir = url.pathname
 
   return (
     <article class="popover-hint">
       <h1>404</h1>
-      <p>{i18n(cfg.locale).pages.error.notFound}</p>
-      <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
+      <p>No record exists here. Either this chapter hasn't been written, or its secrets have yet to be uncovered by the Chosen.</p>
+      <a href={baseDir}>Return to Aspiria</a>
     </article>
   )
 }
